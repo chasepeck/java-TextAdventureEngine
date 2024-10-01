ac4eba1c97870490927513721653f9ca00aeeaac
Chase Peck <52938508+chasepeck@users.noreply.github.com>
2024-09-30 21:20:26 -0400

    simpler

--- Util.java
+++ Util.java
@@ -1,18 +1,15 @@
 final public class Util {
     // Item helpers
     public static Item findItemByName(Item[] items, String name) {
-        for(int i = 0; i < items.length; i++) {
-            if(items[i].name.equals(name)) {   
+        for(int i = 0; i < items.length; i++)
+            if(items[i].name.equals(name))  
                 return items[i];
-            }
-        }
         return null;
     }
     
     // General helpers
     public static void printStringArray(String[] strings) {
-        for(int i = 0; i < strings.length; i++) {
+        for(int i = 0; i < strings.length; i++)
             System.out.println(strings[i]);
-        }
     }
 }
